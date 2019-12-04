26fcbcea81a861e7bc336462a06a6b26b0cd3010
anniepineda <anniepineda@outlook.com>
2019-12-03 23:19:38 -0800

    code-challenges02

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -9,7 +9,6 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
   return `${str} The end.`;
 };
 
@@ -46,7 +45,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  obj.yearborn = year;
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
