85b7534b3154dd08cff74e72aa0986aa18446654
anniepineda <anniepineda@outlook.com>
2020-01-21 13:57:27 -0800

    queu stack challenge

---
+++ code401challenges/src/main/java/code401challenges/stacksandqueues/PseudoQueue.java
@@ -0,0 +1,25 @@
+package code401challenges.stacksandqueues;
+
+public class PseudoQueue {
+    private Stack pushStack;
+    private Stack popStack;
+
+    public PseudoQueue(){
+        this.pushStack = new Stack ();
+        this.popStack =  new Stack ();
+    }
+
+    public void enqueue(int value){
+//        pushStack.push(value);
+        //while enqueue "push" values to dequeue
+
+    }
+
+    public int dequeue(){
+
+
+    }
+
+
+
+}
