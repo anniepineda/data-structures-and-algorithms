99e0547e0f14ad827987d95547a78158583d895a
anniepineda <anniepineda@outlook.com>
2020-01-23 13:43:27 -0800

    setting up challenge

--- code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
+++ code401challenges/src/test/java/code401challenges/ArrayShiftTest.java
@@ -10,7 +10,9 @@ public class ArrayShiftTest {
 
     @Test
     public void insertShiftArray() {
-        ArrayShift.insertShiftArray(new int[]{1,2});
-        System.out.println(Arrays.toString(ArrayShift.insertShiftArray(new int[]{1, 2})));
+//        ArrayShift.insertShiftArray(new int[]{1,2});
+//        System.out.println(Arrays.toString(ArrayShift.insertShiftArray(new int[]{1, 2})));
     }
+
+
 }
