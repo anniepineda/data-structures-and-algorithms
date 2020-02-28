fb4b81f99066471d653fc80ecad23a3fc4bdd818
anniepineda <anniepineda@outlook.com>
2020-02-28 04:47:17 -0800

    mergeSort completed

--- code401challenges/src/main/java/code401challenges/mergeSort/MergeSort.java
+++ code401challenges/src/main/java/code401challenges/mergeSort/MergeSort.java
@@ -1,29 +1,57 @@
 package code401challenges.mergeSort;
 
+import java.util.Arrays;
+
+//Used https://www.youtube.com/watch?v=yv6svAfoYik&t=18s , https://gist.github.com/Kwistech/61cbfad410f132e2e5b70a5a94adfda1 for help
+//Daesy helped me with this challenge and I also referenced code review
+
 public class MergeSort {
+    public static int[] mergeSort(int[] array){
+        int n = array.length;
+        if (n == 0){
+                throw new IllegalArgumentException("The array is empty.");
+            }
+        else if (n == 1){
+                return array;
+        }
 
-    //used https://www.baeldung.com/java-merge-sort to solve challenge
-
-public static void mergeSort(int[] a, int n) {
-    if (n < 2) {
-        return;
-    }
-    int mid = n / 2;
-    int[] l = new int[mid];
-    int[] r = new int[n - mid];
-
-    for (int i = 0; i < mid; i++) {
-        l[i] = a[i];
-    }
-    for (int i = mid; i < n; i++) {
-        r[i - mid] = a[i];
-    }
-    mergeSort(l, mid);
-    mergeSort(r, n - mid);
-
+        else if (n > 1){
+            int mid = n/2;
+            int[] left = Arrays.copyOfRange(array, 0, mid);
+            int[] right = Arrays.copyOfRange(array, mid, n);
+            mergeSort(left);
+            mergeSort(right);
+            merge(left, right, array);
+        }
+        return array;
     }
 
+    /// Helper Method
+    private static void merge(int[] left, int[] right, int[] arr){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                arr[k] = left[i];
+                k++;
+                i++;
+            } else {
+                arr[k] = right[j];
+                k++;
+                j++;
+            }
+        }
+        if(i == left.length){
+            // add value to right
+            while(k < arr.length){
+                arr[k++] = right[j++];
+            }
+            // add value to left
+        } else {
+            arr[k++] = left[i++];
+        }
+    }
 
 }
-
-
