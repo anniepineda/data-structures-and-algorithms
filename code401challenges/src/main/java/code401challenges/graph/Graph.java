0a226f1131f996d0410714bf5a55a7c2e07e9b2d
anniepineda <anniepineda@outlook.com>
2020-03-08 21:17:22 -0700

    code challege done

--- code401challenges/src/main/java/code401challenges/graph/Graph.java
+++ code401challenges/src/main/java/code401challenges/graph/Graph.java
@@ -1,63 +1,142 @@
 package code401challenges.graph;
 
 //import org.junit.Before;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 //Resources: https://www.geeksforgeeks.org/implementing-generic-graph-in-java/
 //https://www.youtube.com/watch?v=gXgEDyodOJU
-//Silas helped me work thru the challenge
 
-
-public class Graph {
-
-    HashSet<Vertex> allVertices;
-
+public class Graph<E> {
+    private Map<Vertex<E>, Set<Edge<E>>> vertices;
     public Graph(){
-        allVertices = new HashSet<>();
+        vertices = new HashMap<>();
     }
-
-    public Vertex addVertex(String nameOfCurrentVertex){
-        Vertex v = new Vertex(nameOfCurrentVertex);
-        v.vertexNodeName = nameOfCurrentVertex;
-        allVertices.add(v);
-        return v;
-    }
-
-    public Edge connectEdgeToVertex(Vertex startOfEdge, Vertex endOfEdge, int weight){
-        Edge pointGoToStart = new Edge(startOfEdge, weight);
-        Edge pointGoToEnd = new Edge(endOfEdge, weight);
-        startOfEdge.edgesOfVertexOrConnections.add(pointGoToEnd);
-        endOfEdge.edgesOfVertexOrConnections.add(pointGoToStart);
-
-        return pointGoToEnd;
-    }
-
-    public ArrayList getVerticesNodes(){
-        ArrayList vertex = new ArrayList();
-        for(Vertex vertexNew : allVertices){
-            vertex.add(vertexNew.vertexNodeName);
+    // AddNode()
+//Adds a new node to the graph
+//Takes in the value of that node
+//Returns the added node
+    public Vertex<E> addNode(E value){
+        if (value != null){
+            Vertex<E> newNode;
+            newNode = new Vertex<>(value);
+            vertices.putIfAbsent(newNode, new HashSet<>());
+            return newNode;
+        } else {
+            throw new IllegalArgumentException("no empty values please");
         }
-        return vertex;
+    }
+    public Edge<E> addEdge(Vertex<E> from, Vertex<E> to, int weight){
+        if (vertices.containsKey(from) && vertices.containsKey(to)) {
+            // create an edge "from" to "to"
+            Edge<E> fromToEdge = new Edge<>(from, to, weight);
+            Set<Edge<E>> getFromNeighbor = vertices.get(from);
+            getFromNeighbor.add(fromToEdge);
+
+            // create an edge "to" to "from"
+            Edge<E> toFromEdge = new Edge<>(to, from, weight);
+            Set<Edge<E>> getToNeighbor = vertices.get(to);
+            getToNeighbor.add(toFromEdge);
+            return fromToEdge;
+        }
+        throw new IllegalArgumentException("no empty values please");
+    }
+    public Set<Vertex<E>> getNodes(){
+        return vertices.keySet();
+    }
+    public Set<Edge<E>> getNeighbors(Vertex<E> vertex){
+        return vertices.get(vertex);
+    }
+    public int getSize(){
+        return vertices.size();
     }
 
-    public List getVertexNeighbors(Vertex allVertexNeighbors){
-        return allVertexNeighbors.edgesOfVertexOrConnections;
+
+    // Breadth First
+
+    public List<Vertex<E>> breadthFirst(Vertex<E> start){
+        List<Vertex<E>> result = new LinkedList<>();
+        Set<Vertex<E>> seen = new HashSet<>();
+        Queue<Vertex<E>> queue = new LinkedList<>();
+//        LinkedList<Graph<E>> nodesToProcess = new LinkedList<>();
+        if(!this.vertices.containsKey(start)){
+            throw new IllegalArgumentException("Vertex outside of graph");
+        }
+
+
+        queue.add(start);
+        seen.add(start);
+        while (!queue.isEmpty()){
+            Vertex<E> current = queue.poll();
+            result.add(current);
+            Set<Edge<E>> neighbors = this.vertices.get(current);
+
+            for(Edge<E> neighbor : neighbors){
+                if(!seen.contains(neighbor)){
+                    seen.add(neighbor.to);
+                    queue.add(neighbor.to);
+                }
+            }
+        }
+        return result;
     }
 
-    //return vertices/nodes in the set
-    public int size(){
-        return allVertices.size();
+
+    //Depth First
+
+    public List<Vertex<E>> depthFirst(Vertex<E> start){
+        List<Vertex<E>> result = new LinkedList<>();
+        Set<Vertex<E>> seen = new HashSet<>();
+        Stack<Vertex<E>> stack = new Stack<>();
+
+        if(!this.vertices.containsKey(start)){
+            throw new IllegalArgumentException("Vertex outside of graph");
+        }
+
+
+        stack.push(start);
+        seen.add(start);
+        while (!stack.isEmpty()){
+            Vertex<E> current = stack.pop();
+            result.add(current);
+            Set<Edge<E>> neighbors = this.vertices.get(current);
+
+            for(Edge<E> neighbor : neighbors){
+                if(!seen.contains(neighbor)){
+                    seen.add(neighbor.to);
+                    stack.add(neighbor.to);
+                }
+            }
+        }
+        return result;
     }
 
-    public String toString(Vertex vertex){
-        return vertex.vertexNodeName;
 
-    }
+    //Lab 37 Alaska Airlines
 
-    public Boolean doesItHaveEdges(Vertex a, Vertex b){
-        return a.edgesOfVertexOrConnections.contains(b.edgesOfVertexOrConnections);
+    public static Connection alaskaAirlines(Graph<String> graph, String[] cities){
+        if(cities.length < 2){
+            throw new IllegalArgumentException("No connecting flights");
+        }
+
+        int totalPrice = 0;
+
+        for(int i = 0; i < cities.length-1; i++){
+            if(!graph.vertices.containsKey(cities[i])){
+                return new Connection(false, 0);
+            }
+            Set<Edge<String>> neighbors = graph.vertices.get(cities[i]);
+            if(!neighbors.contains(cities[i + 1])){
+                return new Connection(false, 0);
+            }
+            for(Edge<String> edge: neighbors){
+                if(edge.to.value.equals(cities[i + 1])){
+                    totalPrice += edge.weight;
+                }
+            }
+
+        }
+        return new Connection(true, totalPrice);
+
     }
 
 
--- code401challenges/src/test/java/code401challenges/graph/GraphTest.java
+++ code401challenges/src/test/java/code401challenges/graph/GraphTest.java
@@ -4,52 +4,97 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 //used https://www.baeldung.com/java-graphs for help
 
 public class GraphTest {
 
+    Graph<Integer> input;
+    Vertex vertex0, vertex1, vertex2, vertex3;
 
-    @Test
-    public void testSize_NodeAdded () {
-        Graph graph = new Graph();
-        graph.addVertex("Strawberry Shortcake");
-        assertEquals("testSize_NodeAdded should return", 1, graph.size());
+    @Before
+    public void initGraph() {
+        input = new Graph<Integer>();
+        vertex0 = input.addNode(0);
+        vertex1 = input.addNode(1);
+        vertex2 = input.addNode(2);
+        vertex3 = input.addNode(3);
     }
 
     @Test
-    public void test_AddEdge () {
-        Graph graph = new Graph();
-        Vertex v1 = graph.addVertex("Red Velvet Cake");
-        Vertex v2 = graph.addVertex("Apple Pie");
-        Edge edge = graph.connectEdgeToVertex(v1, v2, 0);
-        ArrayList expected = new ArrayList<>();
-        expected.add(edge);
-        assertEquals(expected, graph.getVertexNeighbors(v1));
-
+    public void addNodeTest() {
+        int actual = input.getSize();
+        int expected = 4;
+        assertEquals(expected, actual);
     }
-
-
 
     @Test
-    public void test_Size () {
-        Graph graph = new Graph();
-        graph.addVertex("Chocolate Cake");
-        graph.addVertex("Lemon Poppy Seed Cake");
-        assertEquals("Graph contains this many nodes", 2, graph.size());
+    public void addEdgeTest() {
+        Edge edge01 = input.addEdge(vertex0, vertex1, 100);
+        Edge edge02 = input.addEdge(vertex0, vertex2, 100);
+        input.addEdge(vertex1, vertex2, 200);
+        input.addEdge(vertex2, vertex3, 300);
+        Set actual = input.getNeighbors(vertex0);
+        Set expected = new HashSet();
+        expected.add(edge01);
+        expected.add(edge02);
+        assertTrue(actual.containsAll(expected));
     }
-
-
 
     @Test
-    public void test_emptyGraph () {
-        Graph graph = new Graph();
-        assertEquals("There are no nodes", 0, graph.size());
+    public void getNodesTest() {
+        Set actual = input.getNodes();
+        System.out.println("actual = " + actual);
+        assertTrue(actual.contains(vertex0));
+        assertTrue(actual.contains(vertex1));
+        assertTrue(actual.contains(vertex2));
+        assertTrue(actual.contains(vertex3));
     }
-
-
 }
+
+//    @Test
+//    public void testSize_NodeAdded () {
+//        Graph graph = new Graph();
+//        graph.addVertex("Strawberry Shortcake");
+//        assertEquals("testSize_NodeAdded should return", 1, graph.size());
+//    }
+//
+//    @Test
+//    public void test_AddEdge () {
+//        Graph graph = new Graph();
+//        Vertex v1 = graph.addVertex("Red Velvet Cake");
+//        Vertex v2 = graph.addVertex("Apple Pie");
+//        Edge edge = graph.connectEdgeToVertex(v1, v2, 0);
+//        ArrayList expected = new ArrayList<>();
+//        expected.add(edge);
+//        assertEquals(expected, graph.getVertexNeighbors(v1));
+//
+//    }
+//
+//
+//
+//    @Test
+//    public void test_Size () {
+//        Graph graph = new Graph();
+//        graph.addVertex("Chocolate Cake");
+//        graph.addVertex("Lemon Poppy Seed Cake");
+//        assertEquals("Graph contains this many nodes", 2, graph.size());
+//    }
+//
+//
+//
+//    @Test
+//    public void test_emptyGraph () {
+//        Graph graph = new Graph();
+//        assertEquals("There are no nodes", 0, graph.size());
+//    }
+
+
+
 
 
